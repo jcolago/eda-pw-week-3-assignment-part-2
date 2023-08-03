5f72f59366b3877f1ad085e69a65d0252501ffe9
jcolago <jcolago2005@gmail.com>
2023-08-02 22:06:18 -0500

    add countdown loop from 5 to 0

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -37,6 +37,9 @@ for (let i = 2; i <= n; i = i+2){
 
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
+for (let i = 5; i >= 0; i--){
+  console.log(i)
+}
 
 
 // 2. For of loops  
