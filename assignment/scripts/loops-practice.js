41415de15acb5ec43c6ca482bc96bb5448688346
jcolago <jcolago2005@gmail.com>
2023-08-02 22:04:10 -0500

    add even number for loop

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -28,7 +28,12 @@ for (let i = 3; i < 6; i++){
 
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
+// Needed to add another variable for 10. Changed code to: i = 2, i<=n, i = i+2.
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
+let n = 10
+for (let i = 2; i <= n; i = i+2){
+  console.log (i)
+}
 
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
